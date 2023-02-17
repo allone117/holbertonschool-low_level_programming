4a1ae0f06ab0f56f4d7aef44670fed16ed3158fb
allone117 <arclaud89@gmail.com>
2023-02-17 23:14:54 +0000

    gimi the sizes

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,12 +6,12 @@
 
 int main(void)
 {
+    printf("Size of char: %zu byte\n", sizeof(char));
+
     printf("Size of int: %zu bytes\n", sizeof(int));
     printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
     printf("Size of a long long int: %zu byte(s)\n" sizeof(long long int));
-    printf("Size of float: %zu bytes\n", sizeof(float));
-    printf("Size of double: %zu bytes\n", sizeof(double));
-    printf("Size of char: %zu byte\n", sizeof(char));
+    prntf("Size of float: %zu bytes\n", sizeof(float));
     
     return (0);
 
