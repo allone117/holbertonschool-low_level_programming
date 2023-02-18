b9f091550983793b1adc6138d6fe1b755d0a4085
allone117 <arclaud89@gmail.com>
2023-02-18 02:01:41 +0000

    gimi the sizes

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /*
-*main-program that prints the size of various types on the computer it is compiled and run on 
+*main-entry point. 
 *Return: program return to 0.
 */
 int main(void)
