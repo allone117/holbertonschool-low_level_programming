fa8d455e18fc85f687d5a01ff70d0edf4f77d0ac
allone117 <arclaud89@gmail.com>
2023-02-18 01:36:17 +0000

    gimi the sizes

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -4,12 +4,11 @@
 *computer it is compile and run on.
 *Return: program return to 0.
 */
-int main(void)
 {
 printf("Size of char: %d byte(s)\n", sizeof(char));
 printf("Size of int: %d byte(s)\n", sizeof(int));
 printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 printf("Size of float: %d byte(s)\n", sizeof(float));
-return(0);
+return (0);
 }
