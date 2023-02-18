b0cf6bb8daaa8e16f772e0fa5399cbd1d97d7e36
allone117 <arclaud89@gmail.com>
2023-02-18 02:39:41 +0000

    gimi the sizes

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /*
-*main-entry point
+*main-programm
 *Return: program return to 0.
 */
-int main(void)
+	int main(void)
 {
 printf("Size of char: %d byte(s)\n", sizeof(char));
 printf("Size of int: %d byte(s)\n", sizeof(int));
