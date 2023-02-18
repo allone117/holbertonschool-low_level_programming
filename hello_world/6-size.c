a4c72fadebc81e93592fd1a1430fc167af4c627f
allone117 <arclaud89@gmail.com>
2023-02-18 02:47:24 +0000

    gimi the sizes

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -4,7 +4,7 @@
 *Return: program return to 0.
 */
 int main(void)
-{"main"
+{
 printf("Size of char: %d byte(s)\n", sizeof(char));
 printf("Size of int: %d byte(s)\n", sizeof(int));
 printf("Size of a long int: %d byte(s)\n", sizeof(long int));
