f4bbfa1bc603624b66e68b7deccc810bc2c62e13
allone117 <arclaud89@gmail.com>
2023-02-21 23:54:30 +0000

    el alfabet

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,7 +1,7 @@
-#include "main.h"
+#include <stdio.h>
 /**
- * main -  print_alhabet
- * Return: Always 0.
+ * main -  Print the alphabet
+ * Return: Always (Success)
  */
 
 int main(void)
@@ -12,4 +12,5 @@ int main(void)
 	putchar(c);
 	putchar('\n');
 	return (0);
+
 }
