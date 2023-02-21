bddcec85f9cf543963fe834fb057c55732bf677f
allone117 <arclaud89@gmail.com>
2023-02-21 18:02:33 +0000

    if else std

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+
+/**
+ * main - Prints a text acording number
+ * Return: always (Success)
+ *
+ */
+
+int main(void)
+
+{
+	int n, lastd;
+
+	srand(time(0));
+	n = rand() - Rand_max / 2;
+	lasd = n % 10;
+
+	if (lastd > 5)
+	{
+	printf("Last digit of %d is %d and is greater than 5\n", n, lastd);
+	}
+	else if (lastd == 0)
+	{
+	printf("Last digit of %d is %d and is 0\n", n, lastd);
+	}
+	else if (lastd < 6 && lastd != 0)
+	{
+	printf("Last digit of %d is %d and is less than 6 and not 0\n" n, lastd);
+	}
+	return (0);
+
+}
