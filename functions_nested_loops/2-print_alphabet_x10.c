4f8d5d632c9533dabab84464310bd6202eb05b45
allone117 <arclaud89@gmail.com>
2023-02-23 19:37:49 +0000

    abc10

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -5,8 +5,19 @@
  * Return: 0
  */
 
-int main(void)
+void print_alphabet_x10(void)
 {
-	print_alphabet_x10();
-	return (0);i
+char c;
+int i = 0;
+
+while (i <= 9)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+i++;
+}
+
 }
