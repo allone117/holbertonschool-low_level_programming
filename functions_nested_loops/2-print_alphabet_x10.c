2d8ab64cebc714aefcf6d06524b0b2e3fbefd7a4
allone117 <arclaud89@gmail.com>
2023-02-23 19:02:30 +0000

    abc10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - make alphabet 10 times
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
