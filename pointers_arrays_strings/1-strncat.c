a87936e561b63549cee84e17fb2d20394633213d
allone117 <arclaud89@gmail.com>
2023-03-02 22:35:47 +0000

    1strncatq

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -2,11 +2,11 @@
 
 /**
  * _strncat - Concatenates two strings using at most
- * 
+ *
  * an inputted number of bytes from src.
- * 
+ *
  * @dest: The string to be appended upon.
- * 
+ *
  * @src: The string to be appended to dest.
  *
  * @n: The number of bytes from src to be appended to dest.
