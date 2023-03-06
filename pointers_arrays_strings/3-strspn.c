6f097195b197fa71454fd6053c27d74ebbda0786
allone117 <arclaud89@gmail.com>
2023-03-06 17:28:58 +0000

    p

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -9,20 +9,18 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, n, value, check;
+	unsigned int i, n, value;
 
 	value = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-	check = 0;
 
 	for (n = 0; accept[n] != '\0'; n++)
 	{
 	if (accept[n] == s[i])
 	{
 	value++;
-	check = 1;
 	}
 	}
 	}
